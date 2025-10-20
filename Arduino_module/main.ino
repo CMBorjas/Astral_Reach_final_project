ea982050cf08c93e832ed349c2d6cb2da815c30c
qJasonm <117887620+qJasonm@users.noreply.github.com>
2025-10-20 16:33:40 -0600

    Refactor sensor reading and add LED control commands
    Refactored sensor reading logic and cleaned up the code by removing commented-out sections. Added serial command handling for LED control.
    
    Signed-off-by: qJasonm <117887620+qJasonm@users.noreply.github.com>

--- Arduino_module/main.ino
+++ Arduino_module/main.ino
@@ -1,44 +1,64 @@
+int ledPin = 3;
+int brake_led = 3;
 const int trigPin = 9;
 const int echoPin = 10;
-const int brake_led = 3;
-const int lost_led = 7;
-
-long duration, distance;
 float brightness = 0;
+long duration, distance;
+
+long Sensor_Reading() {
+  long dur, dis;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  dur = pulseIn(echoPin, HIGH);
+  dis = (dur/2) / 29.1;
+  delay(10);
+  return dis;
+}
+
 
 void setup() {
+  pinMode(ledPin, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  distance = Sensor_Reading();
+  Serial.println(distance);
+  // if (distance < 10) {
+  //   Serial.println("STOP");
+  //   Serial.println(distance);
+  //   brightness = 255;
+  //   analogWrite(brake_led, brightness);
+  // }
+  // else if (distance > 500){
+  //   Serial.println("LOST");
+  //   analogWrite(brake_led, 0);
+  // }
+  // else{
+  //   Serial.print("Distance: ");
+  //   Serial.println(distance);
+  //   analogWrite(brake_led, 0);
 
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
-  if (distance < 10) {
-    Serial.println("Stop!!!!!!");
-    Serial.println(distance);
-    brightness = 255;
-    digitalWrite(lost_led,LOW);
-    analogWrite(brake_led, brightness);
+  // }
+
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();  // remove spaces or line breaks
+
+    if (command == "ON") {
+      digitalWrite(ledPin, HIGH);
+      // Serial.println("LED is ON");
+    }
+    else if (command == "OFF") {
+      digitalWrite(ledPin, LOW);
+      // Serial.println("LED is OFF");
+    }
   }
-  else if (distance > 500){
-    Serial.println("lost detection");
-    digitalWrite(lost_led,HIGH);
-    analogWrite(brake_led, 0);
-  }
-  else{
-    Serial.print("Distance: ");
-    Serial.println(distance);
-    digitalWrite(lost_led,LOW);
-    analogWrite(brake_led, 0);
-`
-  }
-  delay(10);
 }
+
+
